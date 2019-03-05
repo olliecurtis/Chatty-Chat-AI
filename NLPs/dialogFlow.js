21a08d63454dbb074dacd06f172e6b750c93d0ec
BeigeHacker <BeigeHacker@users.noreply.github.com>
2019-03-05 18:30:26 +0000

    Migrated from apiai legacy to dialogflow api v2

--- NLPs/dialogFlow.js
+++ NLPs/dialogFlow.js
@@ -1,45 +1,67 @@
-const config = require('../secrets');
-// Create an instance of the API.AI using a generated application id
-var apiai = require('apiai')(config.dialogflow.appId);
-
-var drinksAPI = require('../APIs/drinksAPI');
+const config = require("../secrets");
+var dialogflow = require("dialogflow");
+var drinksAPI = require("../APIs/drinksAPI");
 
 /**
  * Method to send conversation to DialogFlow
  * @param {*} text - text input from user
  * @param {*} client - socket to send response
  */
-var dialogFlow = function (text, client) {
-	// Setting the test request for the AI
-	var aiReq = apiai.textRequest(text, {
-		// Our sessionId is a random arbitry string
-		sessionId: config.dialogflow.sessionId
-	});
-	// Sending the AI response back to the UI
-	aiReq.on('response', (response) => {
-		var textRes = response.result.fulfillment.speech;
-		// callAPI(response.result)
-		client.emit('bot reply', callAPI(response.result));
-	});
-	// Show an error to user
-	aiReq.on('error', (error) => {
-		client.emit('bot', "Sorry, there was an error!");
-	});
-	// End the request
-	aiReq.end();
-}
-
-function callAPI(result) {
-	var url = '';
-	switch (result.metadata.intentName) {
-		case "RandomDrink":
-			url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
-			return drinksAPI.api(url);
-		case "SpecficDrink":
-			url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + result.metadata.entities;
-		default:
-			return result.fulfillment.speech;
+var dialogFlow = async function(text, client) {
+	// Setting the config to be used for authenticating the Sessions with DF
+	let conf = {
+		credentials: {
+			private_key: config.dialogflow.private_key.replace(/\\n/g, "\n"),
+			client_email: config.dialogflow.client_email
+		}
 	}
+	// Creating an instance with dialogflow
+  const sessionClient = new dialogflow.SessionsClient(conf);
+  const sessionPath = sessionClient.sessionPath(
+    config.dialogflow.appId,
+    config.dialogflow.sessionId
+  );
+
+  // Making a request to dialogFlow.
+  const aiReq = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: text,
+        languageCode: config.dialogflow.language
+      }
+    }
+  };
+
+	// Call DialogFlow api
+	const aiResp = await sessionClient.detectIntent(aiReq);
+	// If we get a result process it otherwise emit an error
+  if (aiResp[0].queryResult) {
+		client.emit('bot reply', callAPI(aiResp[0].queryResult));
+  } else {
+		client.emit('bot reply', "Sorry, there was an error!");
+  }
+};
+
+/**
+ * @description Function that will call a drinks API for fun
+ * @param {Object} result - The result from DialogFlow
+ * @returns {Object} - Either a message or a drink from the API 
+ */
+function callAPI(result) {
+  var url = "";
+  switch (result.intent.displayName) {
+    case "RandomDrink":
+      url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+      return drinksAPI.api(url);
+    case "SpecficDrink":
+      url =
+        "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" +
+				result.metadata.entities;
+				return drinksAPI.api(url);
+    default:
+      return result.fulfillmentText;
+  }
 }
 
 module.exports.converse = dialogFlow;
--- content/assets/js/script.js
+++ content/assets/js/script.js
@@ -1,10 +1,10 @@
 /**
  *  Chatty AI Bot - Oliver Curtis 2017
- * 
+ *
  *  script.js includes all the code required to capture voice or text input from the 'Human' user
  *  Within this script there are functions to capture events when buttons are pressed and also
  *  handle the posting of the input to the AI API and on successful response deliver to the UI
- * 
+ *
  */
 
 // Invoke an instance of SpeechRecognition, the controller interface of the Web Speech API for voice recognition
@@ -15,80 +15,84 @@ var rec = new SpeechRec();
 var socket = io();
 
 // Send the chat on clicking enter
-document.querySelector('#in-field').addEventListener('keyup', function (e) {
-    if (e.keyCode === 13) {
-        // Obtain the value from the field
-        var input = document.getElementById('in-field').value;
-        // Send the input to the Socket
-        sendChat(input);
-    }
+document.querySelector("#in-field").addEventListener("keyup", function(e) {
+  if (e.keyCode === 13) {
+    // Obtain the value from the field
+    var input = document.getElementById("in-field").value;
+    // Send the input to the Socket
+    sendChat(input);
+  }
 });
 
 // Send the chat input on clicking the message button
-document.querySelector('#message').addEventListener('click', () => {
-    // Obtain the value from the field
-    var input = document.getElementById('in-field').value;
-    // Send the input to the Socket
-    sendChat(input);
+document.querySelector("#message").addEventListener("click", () => {
+  // Obtain the value from the field
+  var input = document.getElementById("in-field").value;
+  // Send the input to the Socket
+  sendChat(input);
 });
 
 // Start speech recognition on button click
-document.querySelector('#listener').addEventListener('click', () => {
-    rec.start();
+document.querySelector("#listener").addEventListener("click", () => {
+  rec.start();
 });
 
 // Retrieve the speech result as text
 // This returns a SpeechRecognitionResult list that contains the result
 // so we can retrieve the text in the array
-rec.addEventListener('result', (event) => {
-    var last = event.results.length - 1;
-    var input = event.results[last][0].transcript;
-    // Send the input to the Socket
-    sendChat(input);
+rec.addEventListener("result", event => {
+  var last = event.results.length - 1;
+  var input = event.results[last][0].transcript;
+  // Send the input to the Socket
+  sendChat(input);
 });
 
 // Stop listening to voice input
-rec.addEventListener('speechend', () => {
-    rec.stop();
+rec.addEventListener("speechend", () => {
+  rec.stop();
 });
 
 // Function that generates a synthetic voice and
 // responds to the user
-socket.on('bot reply', function (reply) {
-    // Instantiate a new instance of Speech 
-    var synth = window.speechSynthesis;
-    // Instantiate a new Speech Utterance - this will create the output voice
-    var utterance = new SpeechSynthesisUtterance();
-    utterance.text = reply;
-    // Respond with voice to the user
-    synth.speak(utterance);
-    // Add the Chatbot response to the page 
-    setChat("Chatty", reply);
+socket.on("bot reply", function(reply) {
+  // Instantiate a new instance of Speech
+  var synth = window.speechSynthesis;
+  // Instantiate a new Speech Utterance - this will create the output voice
+  var utterance = new SpeechSynthesisUtterance();
+  utterance.text = reply;
+  // Respond with voice to the user
+  synth.speak(utterance);
+  // Add the Chatbot response to the page
+  setChat("Chatty", reply);
 });
 
 // Send the chat input on clicking the message button
-document.querySelector('#dialogflow').addEventListener('click', () => {
-    // Send the chat message to the web socket
-    socket.emit('setComponents', "dialogflow");
+document.querySelector("#dialogflow").addEventListener("click", () => {
+  // Send the chat message to the web socket
+  socket.emit("setComponents", "dialogflow");
 });
 // Send the chat input on clicking the message button
-document.querySelector('#luis').addEventListener('click', () => {
-    // Send the chat message to the web socket
-    socket.emit('setComponents', "luis");
+document.querySelector("#luis").addEventListener("click", () => {
+  // Send the chat message to the web socket
+  socket.emit("setComponents", "luis");
 });
 // Send the chat input on clicking the message button
-document.querySelector('#watson').addEventListener('click', () => {
-    // Send the chat message to the web socket
-    socket.emit('setComponents', "watson");
+document.querySelector("#watson").addEventListener("click", () => {
+  // Send the chat message to the web socket
+  socket.emit("setComponents", "watson");
 });
 
 // This function sends the Chat input to the Socket
 // Input: The received message from either speech or text
 function sendChat(textInput) {
+  if (textInput) {
     // Set the input on the screen
     setChat("Human", textInput);
     // Send the chat message to the web socket
-    socket.emit('chat message', textInput);
+    socket.emit("chat message", textInput);
+  } else {
+    // setChat("Error", "You have not provided an input");
+  }
 }
 
 // This function sets the chat message based upon the input
@@ -96,17 +100,18 @@ function sendChat(textInput) {
 //  User: This is either the bot or the human input
 //  Message: This is the input or output from the chat
 function setChat(user, message) {
-    // Create a <p> node
-    var node = document.createElement("P");
-    // Create a text node to hold the message
-    var textnode = document.createTextNode(user + ": " + message);
-    // Add the text node to our new paragraph
-    node.appendChild(textnode);
-    //Add the paragraph to the chat window                
-    document.getElementById("chat-window").appendChild(node);
-    // Here ensures that the chat window scrolls to the bottom
-    document.getElementById("chat-window").scrollTop = document.getElementById("chat-window").scrollHeight;
-    // Reset the input field
-    document.getElementById('in-field').value = '';
-
+  // Create a <p> node
+  var node = document.createElement("P");
+  // Create a text node to hold the message
+  var textnode = document.createTextNode(user + ": " + message);
+  // Add the text node to our new paragraph
+  node.appendChild(textnode);
+  //Add the paragraph to the chat window
+  document.getElementById("chat-window").appendChild(node);
+  // Here ensures that the chat window scrolls to the bottom
+  document.getElementById("chat-window").scrollTop = document.getElementById(
+    "chat-window"
+  ).scrollHeight;
+  // Reset the input field
+  document.getElementById("in-field").value = "";
 }
